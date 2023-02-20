faf2391b9df608aa7bc5427fa4b81542928fed4a
Ranii07 <philnkhata@gmail.com>
2023-02-20 10:36:28 -0800

    alpha reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,2 +1,22 @@
 #include <stdio.h>
+#include <unistd.h>
+/**
+ * Main - Entry point 
+ *
+ * Description: prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0 (success)
+ *
+*/
+int main(void)
+{
+	int ch;
 
+	for (ch = 'z'; ch <= 'a'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
