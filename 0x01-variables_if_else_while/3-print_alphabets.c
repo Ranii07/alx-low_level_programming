2bc26d6b620d88c626da560dea591d5d62dca6a7
Ranii07 <philnkhata@gmail.com>
2023-02-20 00:31:47 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
 /*
  * main - Entry point
  *
- * Description: This program prints the alphabet in lowercase and then uppercase
+ *'This program prints the alphabet in lowercase and then uppercase'
  *
  * Return: Always 0 (success)
  *
