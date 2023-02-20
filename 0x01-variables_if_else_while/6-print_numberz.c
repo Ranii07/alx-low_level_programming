e6a2d00b371917ea5d9f422555f84949eb7c18af
Ranii07 <philnkhata@gmail.com>
2023-02-20 07:14:34 -0800

    commit putchar for numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+ * Main - Entry point
+ *
+ * Description : prints numbers all single
+ * digit numbers
+ */
+int main(void)
+{
+	int n;
+	
+	for (n = 0 ; n <= 9; n++)
+	{
+		putchar(n + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
