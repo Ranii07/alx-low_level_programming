abc03eca7bd5111f63ff9fecc040e28696c07d68
Ranii07 <philnkhata@gmail.com>
2023-02-23 11:42:02 -0800

    commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-        int i;
+//        int i;
 
         printf("Infinite loop incoming :(\n");
 /*
@@ -18,6 +18,5 @@ int main(void)
         }
 */
         printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
+//     	return (0);
 }
