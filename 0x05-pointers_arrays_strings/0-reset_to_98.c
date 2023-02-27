4801d398e661aafa83d2e7105c28a5f8b20ca301
Ranii07 <philnkhata@gmail.com>
2023-02-27 11:08:15 -0800

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ *
+ * 
+*/
+void reset_to_98(int *n)
+{
+	int a;
+
+	a  = 402;
+
+	int *n = &a;
+
+	reset_to_98(&a);
+
+	return (0);
+}
