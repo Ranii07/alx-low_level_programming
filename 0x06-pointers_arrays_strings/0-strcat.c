215ccd7818c2c4e56839fe407c260bd3bb0688b6
Ranii07 <philnkhata@gmail.com>
2023-03-02 05:06:11 -0800

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *
+ * char *_strat() - Write a function that concatenates two strings
+ * @destination : destination array
+ * @source : source array
+ * 
+ * Returns 0 Always
+*/
+
+int main()
+{
+	char destination[] = "Hello ";
+	char source[] = "World!";
+	strcat(destination,source);
+	return (0);
+}
