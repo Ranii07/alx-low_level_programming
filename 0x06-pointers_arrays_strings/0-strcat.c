5332c9ca1b9265395b2d609907904f2ef632875e
Ranii07 <philnkhata@gmail.com>
2023-03-04 10:06:55 -0800

    strct

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,32 @@
 #include "main.h"
-#include <string.h>
+
 /**
+ * _strcat - concatenates two strings.
+ * @dest: dest is a strubg
+ * @src: src is a string (appended word)
  *
- * main - Write a function that concatenates two strings
- * @destination : destination array
- * @source : source array
- * 
- * Returns: 0 Always
+ * Return: Pointer (dest)
 */
 
-int main()
+char *_strcat(char *dest, char *src)
 {
-	char destination[] = "Hello  ";
-	char source[] = "World!";
-	strcat(destination,source);
-	return (0);
+	char *result = dest;
+
+	/* Move dest pointer to the end*/
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	/* Copy the src string to dest*/
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+
+	return (result);
 }
