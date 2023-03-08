4256757646bfb88f4586c4c238f2889b3966e07d
Ranii07 <philnkhata@gmail.com>
2023-03-08 14:22:08 -0800

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints a string followed by new line 
+ *
+ * @*s - is a pointer
+ *
+ * Return: Always 0
+ */
+
+int main()
+{
+	char *s;
+
+	*s = "My string";
+	_puts_recursion(s);
+}
